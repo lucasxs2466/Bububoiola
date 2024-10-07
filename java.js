e12a92b97de921e5a9e16e933daab84f8def7737
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-10-07 07:28:35 -0300

    .

--- java.js
+++ java.js
@@ -1,39 +1,39 @@
-Criacartão('Equações','qual é a fórmula de Bhaskara?','X=-b +- √b^2 -4ac/2')
+CriaCartao('Equações','qual é a fórmula de Bhaskara?','X=-b +- √b^2 -4ac/2')
 
-Criacartão('Equações','qual é a fórmula do gás ideal?','PV=Nrt')
+CriaCartao('Equações','qual é a fórmula do gás ideal?','PV=Nrt')
 
-Criacartão('Equações','fórmula geral da reta','Ax +by + c =0')
+CriaCartao('Equações','fórmula geral da reta','Ax +by + c =0')
 
-Criacartão('Equações','fórmula geral da circunferência','x^2 +y^2 -2ax-2by + a^2+b^2-r^2=0')
+CriaCartao('Equações','fórmula geral da circunferência','x^2 +y^2 -2ax-2by + a^2+b^2-r^2=0')
 
-Criacartão('Química','oq é uma reação de neutralização?','é uma reação entre um ácido e uma base que produz água e um sal ')
+CriaCartao('Química','oq é uma reação de neutralização?','é uma reação entre um ácido e uma base que produz água e um sal ')
 
-Criacartão('Química','qual é a forma do ácido clorídrico?','HCI')
+CriaCartao('Química','qual é a forma do ácido clorídrico?','HCI')
 
-Criacartão('Química','o que é uma reação endotérmica?','é uma reação quimica que absorve energia na forma de calor do ambiente durante o processo')
+CriaCartao('Química','o que é uma reação endotérmica?','é uma reação quimica que absorve energia na forma de calor do ambiente durante o processo')
 
-Criacartão('Química','o que é a tabela periódica dos elementos','A tabela periódica é um arranjo dos elementos químicos com base suas propriedades e número atômico, organizado em linhas(períodos)e colunas(grupos/famílias)')
+CriaCartao('Química','o que é a tabela periódica dos elementos','A tabela periódica é um arranjo dos elementos químicos com base suas propriedades e número atômico, organizado em linhas(períodos)e colunas(grupos/famílias)')
 
-Criacartão('Geografia','o que é a linha do Equador','linha imaginária que divide a terra em hemisférios norte e sul')
+CriaCartao('Geografia','o que é a linha do Equador','linha imaginária que divide a terra em hemisférios norte e sul')
 
-Criacartão('Geografia','o que é o efeito estufa','processo que gases da atmosfera retém calor da terra')
+CriaCartao('Geografia','o que é o efeito estufa','processo que gases da atmosfera retém calor da terra')
 
-Criacartão('Geografia','o que é um Delta','formação geológica criada pela deposição de sedimentos pela foz de um rio')
+CriaCartao('Geografia','o que é um Delta','formação geológica criada pela deposição de sedimentos pela foz de um rio')
 
-Criacartão('Geografia','o que caracteriza um deserto','região com precipitação anual muito baixa')
+CriaCartao('Geografia','o que caracteriza um deserto','região com precipitação anual muito baixa')
 
-Criacartão('Biologia','o que é a fotossíntese?','é o processo pelo qual as plantas convertem luz solar, água e dióxido de carbono em glicose e oxigênio')
+CriaCartao('Biologia','o que é a fotossíntese?','é o processo pelo qual as plantas convertem luz solar, água e dióxido de carbono em glicose e oxigênio')
 
-Criacartão('Biologia','o que são enzimas','enzimas são proteína que acelera as reações químicas no organismo ')
+CriaCartao('Biologia','o que são enzimas','enzimas são proteína que acelera as reações químicas no organismo ')
 
-Criacartão('Biologia','o que são cromossomos','estruturas composta por DNA e proteínas que carregam a informação genética do indivíduo')
+CriaCartao('Biologia','o que são cromossomos','estruturas composta por DNA e proteínas que carregam a informação genética do indivíduo')
 
-Criacartão('Biologia','qual a funčão da mitocôndria','respiração das células')
+CriaCartao('Biologia','qual a funčão da mitocôndria','respiração das células')
 
-Criacartão('História','quem foi o líder da revolução russa em 1917','Vladimir Lênin')
+CriaCartao('História','quem foi o líder da revolução russa em 1917','Vladimir Lênin')
 
-Criacartão('História','o que foi o tratado de Versalhes','acordo que encerrou a primeira guerra mundial em 1919')
+CriaCartao('História','o que foi o tratado de Versalhes','acordo que encerrou a primeira guerra mundial em 1919')
 
-Criacartão('História','o que foi o iluminismo','foi um movimento intelectual do séc XVIII que enfatizou a razão a ciência e o questionamento das autoridades da religião')
+CriaCartao('História','o que foi o iluminismo','foi um movimento intelectual do séc XVIII que enfatizou a razão a ciência e o questionamento das autoridades da religião')
 
-Criacartão('História','o que foram os códigos de Hammurabi','os códigos de Hammurabi foram um dos primeiros conjuntos de leis escritas, promulgadas pelo rei da Babilônia Hammurabi no séc XVIII a.C')
+CriaCartao('História','o que foram os códigos de Hammurabi','os códigos de Hammurabi foram um dos primeiros conjuntos de leis escritas, promulgadas pelo rei da Babilônia Hammurabi no séc XVIII a.C')
